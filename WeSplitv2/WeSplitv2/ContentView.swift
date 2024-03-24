6349d6056f0e1c2bdc4dd8959d0d836c32509b1a
luviein <yenlengggggg@gmail.com>
2024-03-24 23:40:13 +0800

    Add cost splitting function

--- WeSplitv2/WeSplitv2/ContentView.swift
+++ WeSplitv2/WeSplitv2/ContentView.swift
@@ -8,31 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
-    let fruits = ["Apple", "Orange", "Pear"]
-    @State private var selectedFruit = "Apple"
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var focusAmount : Bool
+    @State private var showBreakdown : Bool = false
+    let tips = [9, 0]
     
+    var totalPerPerson : Double {
+        // calculate cost per person
+        let peopleCount = Double(numPeople+2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = (checkAmount / 100) * tipSelection
+        let total = checkAmount + tipValue
+        let amtPerPerson = total / peopleCount
+        return amtPerPerson
+    }
+    
+ 
     var body: some View {
        
         NavigationStack{
             Form{
-                Text("Hello, \(name)")
-                TextField("Type in your name", text: $name)
-                
                 Section{
-                    Picker("Select your fruit", selection: $selectedFruit){
-                        ForEach(fruits, id: \.self){
-                            Text($0)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($focusAmount)
+                    
+                    Picker("Number of people", selection: $numPeople){
+                        ForEach(2..<11){
+                            Text("\($0) people")
                         }
                     }
+                    //.pickerStyle(.navigationLink)
                 }
-                Button("TapCount: \(tapCount)"){
-                    tapCount += 1
+                
+                Section("Is there GST?"){
+                    
+         
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tips, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
+                Section("Each person pays"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
             }
             .navigationTitle("WeSplit")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                if focusAmount {
+                    Button ("Done"){
+                        focusAmount = false
+                    }
+                }
+            }
         }
     }
 }
